eaa700d48d12b963c27cc3a5587a0c1116661b2e
Gurkirat Singh <jaggigs309@gmail.com>
2026-01-20 01:09:43 +0530

    Update agent.py

--- agent.py
+++ agent.py
@@ -93,5 +93,5 @@ def check_availability():
 if __name__ == "__main__":
     while True:
         check_availability()
-        print(" Waiting 30 minutes before next check...\n")
+        print(" Waiting 20 seconds before next check...\n")
         time.sleep(2000)
